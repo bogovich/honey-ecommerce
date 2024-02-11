90d2ac24c128fc8e57b17d6f1ce1bb6bbdc86b2a
pkbogovic <pkbogovic@gmail.com>
2024-02-11 13:40:08 +0100

    Added hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,18 @@
+
+const Hero = () => {
+
+    return (
+        <section className="hero-section">
+          <div className="hero-images">
+              <div className="hero__images-container">
+                  <img src="https://www.turistickeprice.hr/wp-content/uploads/2023/09/foto-Bozo-Boscic-TZ-BBZ.webp" />
+                  <img src="https://source.unsplash.com/zuj7kbZNcUk" />
+              </div>
+              <a className="hero__button hero__button-products">Find more about our area!</a>
+              <a className="hero__button hero__button-locality">Check out our products!</a>
+          </div>
+        </section>
+    );
+};
+
+export default Hero;
