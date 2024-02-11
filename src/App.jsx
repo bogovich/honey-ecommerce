2bfd5b30375c714fefe81a7946f70dd246d9e332
pkbogovic <pkbogovic@gmail.com>
2024-02-11 13:48:20 +0100

    Added react routing and set basic routes for current pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,38 @@
-import './App.css'
-import NavBar from './components/NavBar'
-import Landing from './pages/Landing'
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Landing from "./pages/Landing";
+import Cart from "./pages/Cart";
+import Contact from "./pages/Contact";
+import Shop from "./pages/Shop";
+import { Routes, Route } from "react-router-dom";
 function App() {
-
   return (
     <>
       <header className="App-header">
         <NavBar />
       </header>
       <main className="main">
-        <Landing />
+        <Routes>
+          <Route
+            path="/"
+            element={<Landing />}
+          />
+          <Route
+            path="/cart"
+            element={<Cart />}
+          />
+          <Route
+            path="/contact"
+            element={<Contact />}
+          />
+          <Route
+            path="/shop"
+            element={<Shop />}
+          />
+        </Routes>
       </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
