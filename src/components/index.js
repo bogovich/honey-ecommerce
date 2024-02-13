5ac9463e745270752c0703f40c44ab6394ef1845
pkbogovic <pkbogovic@gmail.com>
2024-02-13 15:32:52 +0100

    Indexed component exports

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+export { default as CartDropdown } from './CartDropdown';
+export { default as CartDropdownItem } from './CartDropdownItem';
+export { default as FilterForm } from './FilterForm';
+export { default as FilterCategoryForm } from './FilterCategoryForm';
+export { default as Hero } from './Hero';
+export { default as NavBar } from './NavBar';
+export { default as Product } from './Product';
+export { default as ProductPage } from './ProductPage';
+export { default as SearchBar } from './SearchBar';
