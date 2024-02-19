e3ec27f305ab4c9a317b5297828373653de0108a
pkbogovic <pkbogovic@gmail.com>
2024-02-19 11:09:36 +0100

    Updated constants in cart, added styles

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,13 +2,13 @@ import { useSelector } from "react-redux";
 import { CartItem } from "../components";
 import { Link } from "react-router-dom";
 import { roundToTwoDecimals } from "../utils";
+import { TAX_RATE, DELIVERY_COST } from "../constants";
 const Cart = () => {
   const cart = useSelector((state) => state.cartReducer.cart);
   const total = useSelector((state) => state.cartReducer.total);
-  const TAX_RATE = 0.25;
   const totalWithoutTax = roundToTwoDecimals(total / (1 + TAX_RATE));
   const totalTax = roundToTwoDecimals(total - totalWithoutTax);
-  const totalWDelivery = roundToTwoDecimals(total + 5);
+  const totalWDelivery = roundToTwoDecimals(total + DELIVERY_COST);
   const cartQuantity = cart.reduce((total, item) => total + item.quantity, 0);
 
   return (
@@ -32,7 +32,7 @@ const Cart = () => {
                 <span>Total w/o tax:</span><span>{totalWithoutTax}€</span>
                 <span>Total tax({TAX_RATE * 100}%):</span><span>{totalTax}€</span>
                 <span>Total w/o delivery:</span><span>{total}€</span>
-                <span>Delivery:</span><span>5.00€</span>
+                <span>Delivery:</span><span>{DELIVERY_COST.toFixed(2)}€</span>
                 <span><strong>Total w/ delivery:</strong></span><span><strong>{totalWDelivery}€</strong></span>
               </div>
               <div className="cart__total-actions">
