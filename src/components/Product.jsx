8b17795eebe104fdf84a51447ac36794fa4238cc
pkbogovic <pkbogovic@gmail.com>
2024-02-21 13:39:26 +0100

    Added spinner while product image is loading

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,11 +1,14 @@
 import { PropTypes } from "prop-types";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import { slugify } from "../utils";
 import AddToCartButton from "./AddToCartButton";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 
 const Product = ({ product }) => {
   const { id, category, images, title, packaging, price } = product;
-
+  const [isImageLoaded, setImageLoaded] = useState(false);
   if(product.category === undefined) return null;
 
   const nameSlug = slugify(title.en);
@@ -14,11 +17,16 @@ const Product = ({ product }) => {
 
   const productLink = `/products/${categorySlug}/${nameSlug}-${packagingSlug}-${id}`;
 
+  const handleImageLoad = () => {
+    setImageLoaded(true);
+  };
+
   return (
     <div className="product">
       <div className="product__img">
-        <Link to={productLink}>
-          <img src={images[1]} alt={title.en} />
+      <Link to={productLink}>
+          {!isImageLoaded && <FontAwesomeIcon className="product__img-loader" icon={faSpinner} spin size="xl" />}
+          <img src={images[1]} alt={title.en} style={{display: isImageLoaded ? 'block' : 'none'}} onLoad={handleImageLoad} />
         </Link>
       </div>
 
