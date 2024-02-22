b512dd96afeae918d765371a47d70e4acf05b372
pkbogovic <pkbogovic@gmail.com>
2024-02-22 15:09:34 +0100

    Serverless update

--- api/categories.js
+++ api/categories.js
@@ -1,5 +1,19 @@
-import db from './firebaseInit';
-import { collection, getDocs } from 'firebase/firestore/lite';
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
 export default async (req, res) => {
     if (req.method === 'GET') {
       const q = collection(db, "categories");
@@ -12,7 +26,6 @@ export default async (req, res) => {
       });
       res.status(200).json(categoriesArray);
     } else {
-      // Handle any other HTTP method
       res.setHeader('Allow', ['GET']);
       res.status(405).end(`Method ${req.method} Not Allowed`);
     }
--- api/products.js
+++ api/products.js
@@ -1,9 +1,20 @@
-import db from './firebaseInit';
-import { collection, getDocs, query, orderBy } from 'firebase/firestore/lite';
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs, query, orderBy } from 'firebase/firestore/lite';
 
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 export default async (req, res) => {
     if (req.method === 'GET') {
-      // Handle GET request
       const q = query(collection(db, "products"), orderBy("created_at", "desc"));
       const data = await getDocs(q);
       const productsArray = await Promise.all(data.docs.map(async (doc) => {
@@ -17,7 +28,6 @@ export default async (req, res) => {
       }));
       res.status(200).json(productsArray);
     } else {
-      // Handle any other HTTP method
       res.setHeader('Allow', ['GET']);
       res.status(405).end(`Method ${req.method} Not Allowed`);
     }
