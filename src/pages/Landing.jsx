516ef3257ff3c60d28c292ffb09d23eaf5d808f2
pkbogovic <pkbogovic@gmail.com>
2024-02-22 18:16:17 +0100

    Added loading for featured products in landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,25 +1,35 @@
-
-import Product from '../components/Product'
-import Hero from '../components/Hero'
-import { useSelector } from 'react-redux'
-import { selectFeaturedProducts } from '../redux/selectors'
+import Product from "../components/Product";
+import Hero from "../components/Hero";
+import { useSelector } from "react-redux";
+import { selectFeaturedProducts } from "../redux/selectors";
+import Loading from "../components/Loading";
 
 const Landing = () => {
-    const featuredProducts = useSelector(selectFeaturedProducts);
+  const featuredProducts = useSelector(selectFeaturedProducts);
+  const status = useSelector((state) => state.productReducer.status);
+  console.log(status);
 
-    return (
-        <>
-            <Hero />
-            <section className="landing__products">
-                <h1 className="hero__title">Featured products</h1>
-                <div className="products">
-                    {featuredProducts.map(product => (
-                        <Product key={product.id} product={product}/>
-                    ))}
-                </div>
-            </section>
-        </>
-        )
-}
+  if (!featuredProducts) {
+    return <h1> Loading </h1>;
+  }
+
+  return (
+    <>
+      <Hero />
+      <section className="landing__products">
+        <h1 className="hero__title">Featured products</h1>
+
+        <div className="products">
+          {status === "loading" && <Loading />}
+
+          {status === "succeeded" &&
+            featuredProducts.map((product) => (
+              <Product key={product.id} product={product} />
+            ))}
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default Landing;
